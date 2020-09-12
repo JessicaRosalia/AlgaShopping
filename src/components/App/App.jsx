67ec75229991031a25f3ec601ebc207f13b063df
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-11 22:22:56 -0300

    chore: adicionei componente de App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Wrapper } from './App.styles'
+
+function App() {
+    return <Wrapper>
+        <Container>todo: app</Container>
+    </Wrapper>
+}
+
+export default App
