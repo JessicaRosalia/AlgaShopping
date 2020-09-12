2fef05d64353ad74e20de60d976fd9b48081d255
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-11 22:52:48 -0300

    chore: Adicionei layout base

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import { Container, Wrapper } from './App.styles'
+import AppHeader from '../AppHeader/AppHeader'
+import AppContainer from '../AppContainer/AppContainer'
 
 function App() {
     return <Wrapper>
-        <Container>todo: app</Container>
+        <Container>
+            <AppHeader />
+            <AppContainer
+                left={
+                    <div style={{ backgroundColor: 'red' }}>produtos disponiveis</div>
+                }
+                middle={
+                    <div style={{ backgroundColor: 'red' }}>sua lista de compras</div>
+                }
+                right={
+                    <div style={{ backgroundColor: 'blue' }}>estatisticas</div>
+                }
+            />
+        </Container>
     </Wrapper>
 }
 
